b4a6a691267e06f9a9ecf083d2acdaa27ddb07f6
green_gotai <tiagomcd.cs@gmail.com>
2023-08-14 11:43:33 -0300

    correçao da funçao scan

--- estruturas-de-controle/estruturas-condicionais/src/CaixaEletronico.java
+++ estruturas-de-controle/estruturas-condicionais/src/CaixaEletronico.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class CaixaEletronico {
     public static void main(String[] args) throws Exception {
         double saldo = 25.0;
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Qual o valor de saque?");
-        double valorSolicitado = scan.nextDouble();
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Qual o valor de saque?");
+            double valorSolicitado = scan.nextDouble();
 
-        if(valorSolicitado <= saldo) {
-            saldo -= valorSolicitado;
-        } else {
-            System.out.println("Saldo insuficiente.");
+            if(valorSolicitado <= saldo) {
+                saldo -= valorSolicitado;
+            } else {
+                System.out.println("Saldo insuficiente.");
+            }
         }
-
         System.out.printf("Saldo atual: %.2f", saldo);
     }
 }
